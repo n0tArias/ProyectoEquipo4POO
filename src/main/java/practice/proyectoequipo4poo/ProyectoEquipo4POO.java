cb599e005043d44f1a97e9381ea2e6b31f95e1d9
AriasCookie <90468069+n0tArias@users.noreply.github.com>
2024-11-07 08:21:14 -0600

    First commit

---
+++ src/main/java/practice/proyectoequipo4poo/ProyectoEquipo4POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package practice.proyectoequipo4poo;
+
+/**
+ *
+ * @author Amigo
+ */
+public class ProyectoEquipo4POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
