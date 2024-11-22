6cf95fa3b26dcba4530511b9cad4fce800ad0f9c
n0tArias <90468069+n0tArias@users.noreply.github.com>
2024-11-22 12:40:03 -0600

    First commit

---
+++ ProyectoEquipo4POO/src/main/java/project/proyectoequipo4poo/ProyectoEquipo4POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package project.proyectoequipo4poo;
+
+/**
+ *
+ * @author n0tArias
+ */
+public class ProyectoEquipo4POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
